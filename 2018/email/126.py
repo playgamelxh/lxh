1359b1597be267cb40b424d0171e00f46d484c87
TooYongTooNative <419006096@qq.com>
2019-02-15 14:58:27 +0800

    add

--- 2018/email/126.py
+++ 2018/email/126.py
@@ -22,12 +22,12 @@ from email.utils import parseaddr
 # mail_smtp_host = "smtp.qq.com"       # 设置smtp服务器
 # mail_pop_host = "pop.qq.com"         # 设置pop服务器
 # mail_user = "330318747@qq.com"       # 用户名
-# mail_pass = "xkhwxtetrzexbhfi"       # 口令,QQ邮箱是输入授权码，在qq邮箱设置 里用验证过的手机发送短信获得，不含空格
+# mail_pass = ""       # 口令,QQ邮箱是输入授权码，在qq邮箱设置 里用验证过的手机发送短信获得，不含空格
 
 mail_smtp_host = "smtp.126.com"       # 设置smtp服务器
 mail_pop_host = "pop.126.com"         # 设置pop服务器
 mail_user = "playgamelxh1@126.com"       # 用户名
-mail_pass = "xkhwxtetrzexbhf1"       # 口令,QQ邮箱是输入授权码，在qq邮箱设置 里用验证过的手机发送短信获得，不含空格
+mail_pass = ""       # 口令,QQ邮箱是输入授权码，在qq邮箱设置 里用验证过的手机发送短信获得，不含空格
 
 '''
 /*DISCRIPTION
